e012b6f4d611823b813aac956875c6d9a6919dcf
buctwbzs <zh_buctwbzs@163.com>
2018-07-03 22:11:20 +0800

    :sparkles:Add ArrayQueue

---
+++ src/main/queue/ArrayQueue.java
@@ -0,0 +1,59 @@
+package main.queue;
+
+import main.array.DynamicArray;
+
+public class ArrayQueue<E> implements Queue<E> {
+
+    private DynamicArray<E> array;
+
+    public ArrayQueue(int capacity) {
+
+        array = new DynamicArray<>(capacity);
+    }
+
+    public ArrayQueue() {
+    }
+
+    @Override
+    public void enqueue(E e) {
+        array.addLast(e);
+    }
+
+    @Override
+    public E dequeue() {
+        return array.removeFirst();
+    }
+
+    @Override
+    public E getFront() {
+        return array.getFirst();
+    }
+
+    @Override
+    public int getSize() {
+        return array.getSize();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return array.isEmpty();
+    }
+
+    public int getCapacity() {
+        return array.getCapacity();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        res.append("Queue: ");
+        res.append("front [");
+        for (int i = 0; i < array.getSize(); i++) {
+            res.append(array.get(i));
+            if (i != array.getSize() - 1)
+                res.append(", ");
+        }
+        res.append("] tail");
+        return super.toString();
+    }
+}
---
+++ src/main/queue/Queue.java
@@ -0,0 +1,14 @@
+package main.queue;
+
+public interface Queue<E> {
+
+    void enqueue(E e);
+
+    E dequeue();
+
+    E getFront();
+
+    int getSize();
+
+    boolean isEmpty();
+}
