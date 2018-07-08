fb28e0b51c3f7c054e7cedf906afab76ef38020e
buctwbzs <zh_buctwbzs@163.com>
2018-07-08 23:31:01 +0800

    :sparkles:Add swap method

--- src/main/array/DynamicArray.java
+++ src/main/array/DynamicArray.java
@@ -2,160 +2,180 @@ package main.array;
 
 public class DynamicArray<E> {
 
-    private E[] data;
-    private int size;
+  private E[] data;
+  private int size;
 
-    public DynamicArray(int capacity) {
+  public DynamicArray(int capacity) {
 
-        data = (E[]) new Object[capacity];
-        size = 0;
+    data = (E[]) new Object[capacity];
+    size = 0;
 
+  }
+
+  public DynamicArray() {
+
+    this(10);
+
+  }
+
+  public int getSize() {
+
+    return size;
+
+  }
+
+  public int getCapacity() {
+
+    return data.length;
+
+  }
+
+  public boolean isEmpty() {
+
+    return size == 0;
+
+  }
+
+  public boolean contains(E e) {
+
+    for (int i = 0; i < size; i++) {
+      if (data[i] == e)
+        return true;
+    }
+    return false;
+  }
+
+  public int find(E e) {
+
+    for (int i = 0; i < size; i++) {
+      if (data[i] == e)
+        return i;
     }
 
-    public DynamicArray() {
+    return -1;
 
-        this(10);
+  }
 
+  public void addFirst(E e) {
+
+    add(0, e);
+
+  }
+
+  public void addLast(E e) {
+
+    add(size, e);
+
+  }
+
+  public void add(int index, E e) {
+
+    if (index < 0 || index > size)
+      throw new IllegalArgumentException("Add Last failed, require index >0 && index < size");
+
+    if (size == data.length) {
+      resize(2 * data.length);
     }
 
-    public int getSize() {
-
-        return size;
-
+    for (int i = size - 1; i >= index; --i) {
+      data[i + 1] = data[i];
     }
+    data[index] = e;
+    ++size;
 
-    public int getCapacity() {
+  }
 
-        return data.length;
+  public E removeFirst() {
 
+    return remove(0);
+
+  }
+
+  public E removeLast() {
+
+    return remove(size - 1);
+
+  }
+
+  public void removeEle(E e) {
+
+    int index = find(e);
+    if (index != -1)
+      remove(index);
+
+  }
+
+  public E remove(int index) {
+
+    if (index < 0 || index >= size)
+      throw new IllegalArgumentException("Remove Failed. index is illegal.");
+
+    E temp = data[index];
+    for (int i = index + 1; i < size; ++i) {
+      data[i - 1] = data[i];
     }
+    --size;
+    data[size] = null; // loitering Object != memory leak
 
-    public boolean isEmpty() {
+    if (size == data.length / 4 && data.length / 2 != 0)
+      resize(data.length / 2);
+    return temp;
 
-        return size == 0;
+  }
 
+  public E getLast() {
+
+    return get(size - 1);
+
+  }
+
+  public E getFirst() {
+
+    return get(0);
+
+  }
+
+  public E get(int index) {
+
+    if (index < 0 || index >= size)
+      throw new IllegalArgumentException("Get Failed. index is illegal.");
+
+    return data[index];
+
+  }
+
+  private void resize(int newCapacity) {
+
+    E[] newData = (E[]) new Object[newCapacity];
+
+    for (int i = 0; i < size; i++)
+      data[i] = data[i];
+    data = newData;
+  }
+
+  public void swap(int i, int j) {
+    if (i < 0 || j < 0 || i >= size || j >= size)
+      throw new IllegalArgumentException("Index is Illegal");
+    E temp = data[i];
+    data[i] = data[j];
+    data[j] = temp;
+  }
+
+  @Override
+  public String toString() {
+
+    StringBuilder res = new StringBuilder();
+    res.append(String.format("Array:size = %d. capacity: capacity=% d"));
+    res.append('[');
+    for (int i = 0; i < size; i++) {
+      res.append(data[i]);
+      if (i != size - 1)
+        res.append(",");
     }
+    res.append("]");
 
-    public boolean contains(E e) {
+    return res.toString();
 
-        for (int i = 0; i < size; i++) {
-            if (data[i] == e)
-                return true;
-        }
-        return false;
-    }
-
-    public int find(E e) {
-
-        for (int i = 0; i < size; i++) {
-            if (data[i] == e)
-                return i;
-        }
-
-        return -1;
-
-    }
-
-    public void addFirst(E e) {
-
-        add(0, e);
-
-    }
-
-    public void addLast(E e) {
-
-        add(size, e);
-
-    }
-
-    public void add(int index, E e) {
-
-        if (index < 0 || index > size)
-            throw new IllegalArgumentException("Add Last failed, require index >0 && index < size");
-
-        if (size == data.length) {
-            resize(2 * data.length);
-        }
-
-        for (int i = size - 1; i >= index; --i) {
-            data[i + 1] = data[i];
-        }
-        data[index] = e;
-        ++size;
-
-    }
-
-    public E removeFirst() {
-
-        return remove(0);
-
-    }
-
-    public E removeLast() {
-
-        return remove(size - 1);
-
-    }
-
-    public void removeEle(E e) {
-
-        int index = find(e);
-        if (index != -1)
-            remove(index);
-
-    }
-
-    public E remove(int index) {
-
-        if (index < 0 || index >= size)
-            throw new IllegalArgumentException("Remove Failed. index is illegal.");
-
-        E temp = data[index];
-        for (int i = index + 1; i < size; ++i) {
-            data[i - 1] = data[i];
-        }
-        --size;
-        data[size] = null; // loitering Object != memory leak
-
-        if (size == data.length / 4 && data.length / 2 != 0)
-            resize(data.length / 2);
-        return temp;
-
-    }
-
-    public E get(int index) {
-
-        if (index < 0 || index >= size)
-            throw new IllegalArgumentException("Get Failed. index is illegal.");
-
-        return data[index];
-
-    }
-
-    private void resize(int newCapacity) {
-
-        E[] newData = (E[]) new Object[newCapacity];
-
-        for (int i = 0; i < size; i++)
-            data[i] = data[i];
-        data = newData;
-    }
-
-    @Override
-    public String toString() {
-
-        StringBuilder res = new StringBuilder();
-        res.append(String.format("Array:size = %d. capacity: capacity=% d"));
-        res.append('[');
-        for (int i = 0; i < size; i++) {
-            res.append(data[i]);
-            if (i != size - 1)
-                res.append(",");
-        }
-        res.append("]");
-
-        return res.toString();
-
-    }
+  }
 }
 
